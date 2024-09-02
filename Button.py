6ba04be8afd57fce67e2878007ac3ccaf9ad1f92
topen0330 <98753980+paperntman@users.noreply.github.com>
2024-09-02 22:52:51 +0900

    removed captions

--- Button.py
+++ Button.py
@@ -2,26 +2,18 @@ import pygame
 
 class Button:
     def __init__(self, image, width=None, height=None, on_click=None, x=0, y=0):
-        # 이미지의 원래 크기를 가져옵니다.
         original_width, original_height = image.get_size()
-
-        # width와 height가 None일 경우 원래 크기를 사용합니다.
         self.width = width if width is not None else original_width
         self.height = height if height is not None else original_height
-
-        # 이미지를 지정된 크기로 변환합니다.
         self.image = pygame.transform.scale(image, (self.width, self.height))
         self.rect = self.image.get_rect()
-
-        # 버튼의 중앙을 기준으로 위치 설정
         self.rect.center = (x, y)
-
-        self.on_click = on_click  # 클릭 시 실행할 함수
+        self.on_click = on_click
 
     def draw(self, screen):
-        screen.blit(self.image, self.rect.topleft)  # rect의 위치를 사용하여 그립니다.
+        screen.blit(self.image, self.rect.topleft)
 
     def check_click(self, pos):
         if self.rect.collidepoint(pos):
             if self.on_click:
-                self.on_click()  # 클릭 시 함수 실행
+                self.on_click()
--- scene_start.py
+++ scene_start.py
@@ -1,9 +1,5 @@
 import pygame
-from pygame.locals import *
-import Util
-
-# Button 클래스를 import합니다.
-from Button import Button  # Button 클래스가 정의된 파일을 import
+from Button import Button
 
 def setup(scene_manager):
     global manager
@@ -13,19 +9,17 @@ def setup(scene_manager):
 
     manager = scene_manager
 
-    # 이미지 파일들을 불러옵니다.
     background = pygame.image.load("assets/background.jpeg")
     background = pygame.transform.scale(background, (1920, 1080))
     logo = pygame.image.load("assets/logo.png")
     logo = pygame.transform.scale(logo, (500, 500))
 
-    # 버튼을 생성합니다. 중앙에 배치되도록 x, y를 설정합니다.
     button_image = pygame.image.load("assets/Buttons.png").subsurface((0, 32, 96, 32))
-    start_button = Button(button_image, on_click=lambda: manager.change_scene('town'), x=960, y=540)  # 중앙 위치
+    start_button = Button(button_image, on_click=lambda: manager.change_scene('town'), x=960, y=540)
 
 def handle_event(event):
-    if event.type == pygame.MOUSEBUTTONDOWN:  # 마우스 왼쪽 버튼을 눌렀다면
-        start_button.check_click(event.pos)  # 버튼 클릭 감지
+    if event.type == pygame.MOUSEBUTTONDOWN:
+        start_button.check_click(event.pos)
 
 def update():
     pass
@@ -33,7 +27,7 @@ def update():
 def draw(screen):
     # 버튼들을 그립니다.
     screen.blit(background, (0, 0))
-    start_button.draw(screen)  # 버튼 그리기
+    start_button.draw(screen)
     screen.blit(logo, (710, 50))
 
 def cleanup():
