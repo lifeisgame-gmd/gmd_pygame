cfa3a9c208e24c04b86d1e219afd3c71bf13a0fc
krolle9 <166526675+krolle9@users.noreply.github.com>
2024-09-13 16:17:25 +0900

    added fight scene
    유희왕 카드를 넣은 건 나 김크뤌이 게이라서 그런 것입니다.

--- scenes/scene_fight.py
+++ scenes/scene_fight.py
@@ -1,5 +1,4 @@
 import pygame
-import pygame
 
 """
 Example Scene
@@ -9,6 +8,13 @@ Example Scene
 def setup(scene_manager):
     global manager
     manager = scene_manager
+    global fight_background
+    global fight_skill_card
+    global fight_skill_card2
+    fight_background = pygame.image.load("assets/fight/fight_bg.jpg")
+    fight_background = pygame.transform.scale(fight_background, (1920, 1080))
+    fight_skill_card=pygame.image.load("assets/fight/card1.jpg")
+    fight_skill_card2=pygame.image.load("assets/fight/card2.jpg")
 
 # 씬이 불러와진 상태일 때, 이벤트가 작동할 시 실행되는 메소드입니다.
 def handle_event(event):
@@ -20,7 +26,9 @@ def update():
 
 # 씬이 불러와진 상태일 때, 각 프레임마다 update 메소드 뒤에 실행되는 메소드입니다.
 def draw(screen):
-    pass
+    screen.blit(fight_background, (0, 0))
+    screen.blit(fight_skill_card, (960,780))
+    screen.blit(fight_skill_card2, (830,780))
 
 # 다른 씬으로 넘어갈 때 실행되는 메소드입니다.
 def cleanup():
