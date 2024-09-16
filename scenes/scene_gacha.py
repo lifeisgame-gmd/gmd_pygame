c56ce0ea9db81d9bd1f20e328cd11cee04e06217
Myakmyak <parkjihoosoda@gmail.com>
2024-09-16 21:20:36 +0900

    가챠 UI 끝, 랜덤 돌리는거는 람다 추가로 넣어줘야함.
    랜덤 돌리는거 람다 저거 플레이어블 캐릭터 리스트 있으면 그 안에서 import random 의 random.choice(리스트이름) 쓰면 될 것 같음. 그리고 for 문 써서 그 캐릭터 이미지 소화해주면 될듯?

--- scenes/scene_gacha.py
+++ scenes/scene_gacha.py
@@ -1,4 +1,5 @@
 import pygame
+import random
 from util.Button import Button
 
 from SceneManager import SceneManager
@@ -18,7 +19,6 @@ def setup(scene_manager):
     global recruitment_button
     global back_button
     global greeting_text
-    global Hero_Description
 
     manager = scene_manager
     background = pygame.image.load("assets/gacha/gacha_background.png") #배경
@@ -26,16 +26,17 @@ def setup(scene_manager):
     Button_image = pygame.image.load("assets/button.png")
     Button_image = pygame.transform.scale(Button_image, (300, 100))
     back_button = Button(Button_image, on_click=lambda: manager.change_scene('town'), x=1750, y=1010, is_center=True)
-    Hero_Description = pygame.Rect(0, 0, 300, 1500)
+    recruitment_button = Button(Button_image, on_click=lambda: manager.change_scene('town'), x=1000, y=900, is_center=True)
 
     #폰트 / 글자
     game_font = pygame.font.Font(r"assets\font\빛의 계승자 Regular\HeirofLightRegular.ttf", 100)
-    greeting_text = game_font.render("여관.", True,(0,0,0)) #("텍스트",안티에일어싱 여부,(R,G,B) 또는 색깔 대문자로 써 넣기.)
+    greeting_text = game_font.render("여관", True,(0,0,0)) #("텍스트",안티에일어싱 여부,(R,G,B) 또는 색깔 대문자로 써 넣기.)
 
 # 씬이 불러와진 상태일 때, 이벤트가 작동할 시 실행되는 메소드입니다.
 def handle_event(event):
     if event.type == pygame.MOUSEBUTTONDOWN:
         back_button.check_click(event.pos)
+        recruitment_button.check_click(event.pos)
     pass
 
 # 씬이 불러와진 상태일 때, 각 프레임마다 실행되는 메소드입니다.
@@ -46,6 +47,7 @@ def update():
 def draw(screen):
     screen.blit(background,(0,0))
     back_button.draw(screen)
+    recruitment_button.draw(screen)
     screen.blit(greeting_text,(0,0))
     pygame.draw.rect(screen, (255,255,255),[10, 150, 500, 800]) #사각형 그리기 / 설명창
     pygame.draw.ellipse(screen, (0,0,255),[760,500,500,300]) #타원 그리기 / 대충 몬스터 그려봄 ㅇㅇ.
