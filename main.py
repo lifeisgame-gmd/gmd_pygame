43fdebf9f76a3648b59b80dee7d3048adf91691a
topen0330 <98753980+paperntman@users.noreply.github.com>
2024-09-16 20:08:22 +0900

    sasdf

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
 import pygame
-import ctypes
-
-u32 = ctypes.windll.user32
-resolution = u32.GetSystemMetrics(0), u32.GetSystemMetrics(1)
 
 from SceneManager import SceneManager
 
@@ -10,7 +6,10 @@ from SceneManager import SceneManager
 
 if __name__ == "__main__":
     pygame.init()
-    screen = pygame.display.set_mode(resolution,pygame.FULLSCREEN)  # 창 크기 조절 가능
-    scene_manager = SceneManager(screen)
+
+    info = pygame.display.Info()
+    screen = pygame.display.set_mode((info.current_w, info.current_h), pygame.NOFRAME)
+    virtual_screen = pygame.Surface((1920, 1080))
+    scene_manager = SceneManager(screen, virtual_screen)
     scene_manager.run()
     pygame.quit()
