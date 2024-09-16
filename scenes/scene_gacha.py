bf36407e85699991d50dfb861655054a5585d003
Myakmyak <parkjihoosoda@gmail.com>
2024-09-16 20:26:25 +0900

    빛의 계승자 폰트 추가 + 길드 폰트(빛계)사용

--- scenes/scene_gacha.py
+++ scenes/scene_gacha.py
@@ -16,6 +16,7 @@ def setup(scene_manager):
     global background
     global recruitment_button
     global back_button
+    global greeting_text
 
     manager = scene_manager
     background = pygame.image.load("assets/gacha/gacha_background.png") #배경
@@ -24,6 +25,9 @@ def setup(scene_manager):
     Button_image = pygame.transform.scale(Button_image, (300, 100))
     back_button = Button(Button_image, on_click=lambda: manager.change_scene('town'), x=1750, y=1010, is_center=True)
 
+    #폰트 / 글자
+    game_font = pygame.font.Font(r"assets\font\빛의 계승자 Regular\HeirofLightRegular.ttf", 100)
+    greeting_text = game_font.render("여관.", True,(0,0,0)) #("텍스트",안티에일어싱 여부,(R,G,B) 또는 색깔 대문자로 써 넣기.)
 
 # 씬이 불러와진 상태일 때, 이벤트가 작동할 시 실행되는 메소드입니다.
 def handle_event(event):
@@ -39,6 +43,7 @@ def update():
 def draw(screen):
     screen.blit(background,(0,0))
     back_button.draw(screen)
+    screen.blit(greeting_text,(0,0))
     pass
 
 # 다른 씬으로 넘어갈 때 실행되는 메소드입니다.
