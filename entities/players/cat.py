dc0fcfa2b1d18acae5267fc0c0ee351d510fbb31
topen0330 <98753980+paperntman@users.noreply.github.com>
2024-10-20 01:26:06 +0900

    added asset

--- entities/players/cat.py
+++ entities/players/cat.py
@@ -20,7 +20,7 @@ class Cat(Player):
         return additional_data.name + "을(를) " + str(self.atk)+"의 데미지로 공격했다!"
 
     skills = [
-        Skill('cat_atk', '냥냥 펀치', '지정한 적을 공격합니다.', "assets/fight/card1.jpg", attack, Need.Enemy),
+        Skill('cat_atk', '냥냥 펀치', '지정한 적을 공격합니다.', "assets/player/loli.jpg", attack, Need.Enemy),
         Skill('cat_def', '냥냥 방어', '털로 몸을 감싸 방어합니다.', "assets/player/no_img.png", defend, Need.Self)
     ]
 
