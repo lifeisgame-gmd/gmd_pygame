fcc49a435c6a3f2253e243df7cbc2c1a5f868ca6
Myakmyak <91679862+myakmyak1011@users.noreply.github.com>
2024-10-21 09:03:17 +0900

    holyknight.py 업뎃, 미완

--- entities/players/holyknight.py
+++ entities/players/holyknight.py
@@ -17,21 +17,20 @@ class HolyKnight(Player):
         self.buff_time = fight_data.turn + 3
         
         self.protect += self.hp_m * (3/10) 
-
         return "성기사의 방어력이 증가했다!"
     
-    def defend(self, fight_data, additional_data):
-        self.protect += 5
-        return "성기사의 방어력이 5 증가했다!"
+    def Ally_defend(self, fight_data, additional_data):
+        self.protect += self.hp_m * (2/10)
+        return "성기사의 방어력이 "+ self.hp_m * (2/10)+  "만큼 증가했다!"
 
     def attack(self, fight_data: FightData, additional_data: Entity):
         additional_data.damage(self.atk)
         return additional_data.name + "을(를) " + str(self.atk)+"의 데미지로 공격했다!" 
 
     skills = [
-        Skill('holyknight_atk', '냥냥 펀치', '지정한 적을 공격합니다.', "assets/fight/card1.jpg", attack, Need.Enemy),
-        Skill('holyknight_def', '냥냥 방어', '털로 몸을 감싸 방어합니다.', "assets/player/no_img.png", defend, Need.Self),
-        Skill('holyknight_atk', '신의 ', '지정한 적을 공격합니다.', "assets/fight/card1.jpg", buff, Need.Self)
+        Skill('holyknight_atk', '공격', '지정한 적을 공격합니다.', "assets/fight/card1.jpg", attack, Need.Enemy),
+        Skill('holyknight_guard', '수호', '지정한 아군에게 보호막 증가.', "assets/player/no_img.png", Ally_defend, Need.Ally),
+        Skill('holyknight_buff', '신의 방패', '방어 증가.', "assets/fight/card1.jpg", buff, Need.Self)
     ]
 
     def turn(self, fight_data,buff_time,original_atk):
