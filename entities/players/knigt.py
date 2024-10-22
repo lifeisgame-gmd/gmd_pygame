e6d29442493d9cd8714e5027dd7caf8524b32667
electric530 <youngjoon0530@gmail.com>
2024-10-22 13:30:13 +0900

    기사 스킬변경, 돌 추가
    돌은 단단해~

--- entities/players/knigt.py
+++ entities/players/knigt.py
@@ -8,19 +8,19 @@ from util.Skill import Skill, Need
 class Knight(Player):
 
     def __init__(self):
-        super().__init__('성기사',"assets/player/cat.png", 10, 4, 1)
+        super().__init__('기사',"assets/player/cat.png", 10, 4, 1)
 
 
     def defend(self, fight_data, additional_data):
         self.protect += 10
-        return "성기사의 방어력이 10 증가했다!"
+        return "기사의 방어력이 10 증가했다!"
 
-    def attack(self, fight_data: FightData, additional_data: Entity):
-        additional_data.damage(self.atk)
+    def defend(self, fight_data, additional_data):# 도발 스킬로 
+        self.protect += 10                           #다음턴의 적이 주는 데미지를 모두 이 케릭터가 받음.
         return additional_data.name + "을(를) " + str(self.atk)+"의 데미지로 공격했다!"
 
     skills = [
-        Skill('Knight_atk', '심판', '지정한 적을 공격합니다.', "assets/fight/card1.jpg", attack, Need.Enemy),#최대체력의 15퍼센트, 2턴 후 부터 사용 가능, 4턴마다 1번씩
+        Skill('Knight_all_def', '도발', '다음턴의 공격을 모두 자신이 받습니다.', "assets/fight/card1.jpg", defend, Need.Self),
         Skill('Knight_def', '막기', '방패로 막아 방어합니다.', "assets/player/no_img.png", defend, Need.Self)
     ]
 
