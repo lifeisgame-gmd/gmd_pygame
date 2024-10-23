2acb3b0a7c9e51f4f6397ae9fedd8487019b45a3
myakmyak <parkjihoosoda@gmail.com>
2024-10-23 10:42:14 +0900

    동물 조련사 / 동물들 불러내는 느낌으로 작업할듯. 동물들마다 스킬 다름. 현재 개 70 / 호랑이 25/ 용 5 임.

--- entities/players/animal_trainer.py
+++ entities/players/animal_trainer.py
@@ -1,26 +1,34 @@
 from typing import Any
+import random
 
 from managers.Entities import Player, Entity
 from util.FightData import FightData
 from util.Skill import Skill, Need
 
 
-class Cat(Player):
+class Animal_Trainer(Player):
 
     def __init__(self):
-        super().__init__('고양이',"assets/player/cat.png", 10, 4)
+        super().__init__('동물조련사',"assets/player/cat.png", 10, 4)
+        self.animal = ""
 
 
-    def defend(self, fight_data, additional_data):
-        self.protect += 5
-        return "고양이의 방어력이 5 증가했다!"
+    def call_animal(self, fight_data, additional_data):
+        R = random(1,100)
+        if R <= 70:
+          self.animal = "개"
+        else if R <= 95:
+          self.animal = "호랑이"
+        else:
+          self.animal = "용"
+        return "동물 조련사는 " +self.animal + "을(를) 불러냈다!"
 
     def attack(self, fight_data: FightData, additional_data: Entity):
         additional_data.damage(self.atk)
         return additional_data.name + "을(를) " + str(self.atk)+"의 데미지로 공격했다!"
 
     skills = [
-        Skill('cat_atk', '냥냥 펀치', '지정한 적을 공격합니다.', "assets/player/loli.jpg", attack, Need.Enemy),
+        Skill('cat_atk', '냥냥 펀치', '지정한 적을 공격합니다.', "assets/player/loli.jpg", call_animal, Need.Enemy),
         Skill('cat_def', '냥냥 방어', '털로 몸을 감싸 방어합니다.', "assets/player/no_img.png", defend, Need.Self)
     ]
 
