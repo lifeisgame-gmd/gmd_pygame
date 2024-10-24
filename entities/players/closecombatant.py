0bce504b7f1da90c96ed33c7c7fbf1a5be1ed12d
topen0330 <98753980+paperntman@users.noreply.github.com>
2024-10-24 18:24:58 +0900

    random 오류 제거, 오타 제거

--- entities/players/closecombatant.py
+++ entities/players/closecombatant.py
@@ -8,11 +8,11 @@ from util.Skill import Skill, Need
 class Closecombatant(Player):
 
     def __init__(self):
-        super().__init('근접전투원', "assests/player/closecombantant.png", 20,65,1)
+        super().__init__('근접전투원', "assests/player/closecombantant.png", 20,65)
 
 
     def neckattack(self, fight_data: FightData, additional_data: Entity):
-        r = random(0,70)
+        r = random.randrange(0,71)
         additional_data.damage(self.atk + r)
         return "근접전투원이 적군을" + str(self.atk + r)+"의 데미지로 공격했다!"
 
@@ -26,6 +26,6 @@ class Closecombatant(Player):
     
 
     skills = [
-        Skill('closecombantant_atk', '넥슬라이스', '지정한 적의 목을 벱니다.', "assets/fight/card1.jpg", attack, Need.Enemy),
+        Skill('closecombantant_atk', '넥슬라이스', '지정한 적의 목을 벱니다.', "assets/fight/card1.jpg", neckattack, Need.Enemy),
         Skill('closecombantant_defend', '집중 방어', '무기를 방패삼아 적의 공격을 방어합니다.', "assets/player/no_img.png", defend, Need.Self)
     ]
