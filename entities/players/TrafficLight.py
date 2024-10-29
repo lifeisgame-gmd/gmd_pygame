5e0b91fe89938af1797e79bad5b53d9cf52ec3c2
topen0330 <98753980+paperntman@users.noreply.github.com>
2024-10-29 13:15:28 +0900

    이름 변경
    파스칼 표기법(각 문자의 첫 글자를 대문자로 표시)으로 파일을 표기하였습니다.

--- entities/players/TrafficLight.py
+++ entities/players/TrafficLight.py
@@ -25,7 +25,7 @@ class Cat(Player):
 
     skills = [
         Skill('shd_atk', '초록불', '지정한 적을 공격합니다.', "assets/fight/card1.jpg", attack, Need.Enemy),
-        Skill('shd_cc', '정지', '빨간불이 되어 상대가 다음 신호를 기다립니다.', "assets/player/no_img.png", attack, Need.Enemy)#지정한 한명 스턴넣기
+        Skill('shd_cc', '정지', '빨간불이 되어 상대가 다음 신호를 기다립니다.', "assets/player/no_img.png", attack, Need.Enemy), #지정한 한명 스턴넣기
         Skill('shd_def', '빨간불', '정지하여 자신을 보호합니다.', "assets/player/no_img.png", defend, Need.Self)
     ]
 
