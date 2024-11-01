0dd3ddd466c001990ccc8b69b0885a1e5dfac7dc
Myakmyak <91679862+myakmyak1011@users.noreply.github.com>
2024-11-01 15:15:24 +0900

    동물 조련사 2개 있어서 하나지움

--- entities/players/animal_trainer.py
+++ entities/players/animal_trainer.py
@@ -5,13 +5,15 @@ from managers.Entities import Player, Entity
 from util.FightData import FightData
 from util.Skill import Skill, Need
 
+global target
+target = [0,0,0,0]
 
 class Animal_Trainer(Player):
 
     def __init__(self):
         super().__init__('동물조련사',"assets/player/cat.png", 2, 4)
         self.animal = ""
-        self.enemy_buff_time = 0
+        self.enemy_buff_time = [0,0,0,0]
 
 
     def call_animal(self, fight_data, additional_data):
@@ -25,7 +27,7 @@ class Animal_Trainer(Player):
             del self.skills[2]          
           self.skills.append(Skill('bite','물어!','지정한 상대 1명을 공격한다.',"assets/player/no_img",bite, Need.Enemy))
           self.skills.append(Skill('scar_licking','상처핥기','스스로의 hp를 약간 회복한다.',"assets/player/no_img",licking, Need.self))
-        else if R <= 95:
+        elif R <= 95:
           self.animal = "호랑이"
           if len(self.skills)==2:
             del self.skills [1]
@@ -44,17 +46,17 @@ class Animal_Trainer(Player):
         
     def licking(self, fight_data: FightData, additional_data):
       R = random(1,3)
-      if R == 1
+      if R == 1:
         if self.atk * (45/100)<1:
           self.hp_c += 1
         else:
           self.hp_c += self.atk * (45/100)
-      else if R == 2
+      elif R == 2:
         if self.atk * (50/100)<1:
           self.hp_c += 1
         else:
           self.hp_c += self.atk * (60/100)
-      else if R == 3
+      elif R == 3:
         if self.atk * (60/100)<1:
           self.hp_c += 1
         else:
@@ -66,16 +68,19 @@ class Animal_Trainer(Player):
       R = random(1,3)
       if R == 1:
         additional_data.damage(self.atk*1.5)
-      else if R == 2:
+      elif R == 2:
         additional_data.damage(self.atk*1.7)
       else:
         additional_data.damage(self.atk*2)
       
     def growling(self, fight_data:FightData, additional_data: Entity):
+      for i in range(0,4):
+        if target[i] ==
       self.original_enemy_atk = additional_data.atk
       additional_data.atk(additional_data.atk * (8/10) )
-      self.enemy_buff_time = Fight_data.turn + 4
+      self.enemy_buff_time = fight_data.turn + 4
       return additional_data.name + "의 공격력을 " + additional_data.atk * (8/10) + "만큼 감소시켰다!"
+    
     def attack(self, fight_data: FightData, additional_data: Entity):
         additional_data.damage(self.atk)
         return additional_data.name + "을(를) " + str(self.atk)+"의 데미지로 공격했다!"
@@ -85,8 +90,9 @@ class Animal_Trainer(Player):
         Skill('whip', '채찍질하기', '동물조련사가 상대방을 공격합니다.', "assets/player/no_img.png", attack, Need.Enemy)
     ]
     
-    def turn(self, fight_data,buff_time,original_atk):
-        if self.enemy_buff_time == fight_data.turn:
-            additional_data.atk additional_data.enemy_atk
+    def turn(self, fight_data):
+        global target
+        if self.enemy_buff_time == fight_data.turn and target is not None:
+            target.atk = target.atk_o
             self.enemy_buff_time = 0
         pass
